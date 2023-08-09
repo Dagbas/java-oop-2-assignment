efb82a58d162d1a3410b172a386b0008b19173f7
oscarwani <oscar.i.wani@gmail.com>
2023-08-08 23:39:30 -0400

    adding changes as directed

--- src/mobilePhone/Main.java
+++ src/mobilePhone/Main.java
@@ -3,16 +3,19 @@ package mobilePhone;
 public class Main {
     public static void main(String[] args) {
     	
-        // Instantiate a brand new MobilePhone object and pass it the 4 parameters for your phone (Brand, Model, Phone Number, and Storage Capacity)
-
+        // Instantiate a brand new MobilePhone object and pass it the 4 parameters for your phone (Brand, Model, Phone Number, and Storage Capacity)//
+        MobilePhone newPhonewhothis = new MobilePhone("Samsung", "Galaxy s23 ", 5553218020, 64);
         
         // Call the makeCall() method using your phone object - remember to pass it a phone number as input!
+        newPhonewhothis.makeCall(2813308004);
 
         
         // Call the installApp() method using your phone object - remember to pass it an app name as input!
+        newPhonewhothis.InstallApp("NordVPN");
 
         
         // Display your phones information using the displayInfo() method!
+        newPhonewhothis.displayInfo();
 
     }
 }
--- src/mobilePhone/MobilePhone.java
+++ src/mobilePhone/MobilePhone.java
@@ -8,11 +8,26 @@ public class MobilePhone {
 	// 4.Storage Capacity
 	// Think about what data types would work best with these fields :)
 
+    public String Brand;
+    public String Model;
+    public int PhoneNumber;
+    public int StorageCapacity;
+
+
 
 	// Let's create a public constructor for our MobilePhone class
 	// Given 4 pieces of information as input (brand, model, operatingSystem, and
 	// storageCapacity),
 	// Set the instance variables of your object accordingly
+
+    public MobilePhone (String brand, String model, String operatingSystem, int storageCapacity)
+    {
+        this.Brand= brand;
+        this.Model = model;
+        this.Storage =storageCapacity;
+        this.OS= operatingSystem;
+
+    }
 
 	
 	
@@ -20,20 +35,59 @@ public class MobilePhone {
 	// Please at least write 1 set of getter/setter by hand. After you get the
 	// hang of it, you can go to 'source'->'generate getters and setters'->checkmark
 	// all of the fields you want
-	// getters and setters for->'generate'
 
+    public String getModel() {
+        return Model;
+    }
 
-	// Lets make a couple custom phone-related methods:
+    public int getStorageCapacity() {
+        return StorageCapacity;
+    }
+
+    public void setStorageCapacity(int storage) {
+        StorageCapacity = storage;
+    }
+
+    public int getPhoneNumber() {
+        return Phone;
+    }
+
+    public void setPhoneNumber(int phone) {
+        Phone = phone;
+    }
+
+    public void setBrand(String brand) {
+        Brand = brand;
+    }
+    // getters and setters for->'generate'
+
+    public String getBrand ()
+    {
+        return Brand;
+    }
+
+    public void setModel(String model) {
+        Model = model;
+    }
+
+    // Lets make a couple custom phone-related methods:
 
 	// 1. A makeCall method that takes a phone number as a parameter and then
 	// prints:
-	// 'We are calling {the input phone number} from our {brand} {model} phone!'
+	// ''We are calling {the input phone number} from our {brand} {model} phone!'
+
+    public void makeCall (int inputPhone) {
+        System.out.printf("We are calling %s from our %s %s phone!", inputPhone, Brand, Model);
+    }
 
 
 	
 	// 2. An installApp method that takes an app name String as input and then
 	// prints:
 	// 'Installing {input app name} on our {brand} {model} phone!'
+    public void installApp ( String appName) {
+        System.out.printf("Installing %s on our %s %s phone !", appName, Brand, Model);
+    }
 
 
 	
@@ -44,7 +98,11 @@ public class MobilePhone {
 	// Model: {model}
 	// Phone Number: {phone number}
 	// Storage Capacity: {storage capacity}GB
+    public void displayInfo() {
+        System.out.printf("Brand: %s",Brand);
+        System.out.printf("Model: %s", Model);
+        System.out.printf("Phone Number: %d", PhoneNumber);
+        System.out.printf("Storage Capacity: %d GB", Storage );
 
 
-
-}
+    }
